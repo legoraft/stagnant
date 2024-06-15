997185c48bd9a15785efb7f45049181788d28d77
legoraft <legoraft@legoraft.com>
2024-06-15 14:41:54 +0200

    chore: removed test files

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::{fs, path::Path};
 
 mod post_generator;
 
-fn generator() {
+pub fn generator() {
     let posts = fs::read_dir("./posts").expect("Couldn't find post directory!");
     let posts_template = fs::read_to_string("./template/posts/[id].html").expect("Post template doesn't exist.");
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use pulldown_cmark::Options;
+use std::env;
+
+use stagnant::generator;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     
+    env::set_current_dir(&args[1]).expect("Couldn't move into specified directory!");
+    generator();
 }
