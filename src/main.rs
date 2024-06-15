e77d7ab26f09b55d757b8e56a508d43d2380d15c
legoraft <legoraft@legoraft.com>
2024-06-15 21:56:21 +0200

    fix: stagnant can be run without args

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ use stagnant::generator;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    if args.len() != 2 {
-        eprintln!("Add a site directory as argument.");
+    if args.len() < 2 {
+        generator();
+    } else if args.len() == 2 {
+        env::set_current_dir(&args[1]).expect("Couldn't move into specified directory!");
+        generator();
+    } else {
+        eprintln!("Only add a site directory as argument.");
     }
-    
-    env::set_current_dir(&args[1]).expect("Couldn't move into specified directory!");
-    generator();
+
 }
