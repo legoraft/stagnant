81611dcca917bd6ab4736e5fc63cdb9a196ad79c
legoraft <legoraft@legoraft.com>
2024-06-16 18:03:45 +0200

    test: added tempfiles and test

--- src/template.rs
+++ src/template.rs
@@ -1,5 +1,17 @@
-pub fn copy_directory(source: &Path, destination: &Path) {
+use std::{fs, path::Path};
+
+fn copy_directory(source: &Path, destination: &Path) {
     if !destination.exists() {
+        fs::create_dir_all(destination)?;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_directory_copies() {
         
     }
 }
