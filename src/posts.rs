8242224a007cf9be841c5ebda099a2a385ad4528
legoraft <legoraft@legoraft.com>
2024-06-17 14:50:49 +0200

    refactor: markdown parsing is smaller

--- src/posts.rs
+++ src/posts.rs
@@ -2,7 +2,7 @@ use std::{env::{current_dir, set_current_dir}, fs::{self, ReadDir}, path::{Path,
 
 use pulldown_cmark::Options;
 
-use crate::frontmatter::parse;
+use crate::frontmatter::{parse, split_markdown};
 
 pub fn generate(posts: ReadDir, template: String) {
     let working_dir = current_dir().expect("Working directory is nonexistent.");
@@ -19,30 +19,23 @@ fn write_posts(posts: ReadDir, template: String, working_dir: PathBuf, site_post
 
         let path = post.expect("Couldn't get post file path!").path();
         let file = fs::read_to_string(&path).expect("Couldn't read markdown file!");
-        let (frontmatter, content) = 
+        let (frontmatter, content) = split_markdown(file);
         
-        let html = template
-            .replace("{content}", &content)
-            .replace("{description}", data.description.as_str())
-            .replace("{date}", data.date.as_str())
-            .replace("{title}", data.title.as_str());
+        let html = parse_markdown(&content);
 
         let filename = path.file_stem().unwrap();
         let output_file = [filename.to_str().unwrap(), ".html"].concat();
-        
+
         set_current_dir(&site_posts_dir).expect("Couldn't move into site posts dir!");
         fs::write(output_file, html).expect("Couldn't write post file!");
     }
 }
 
-fn parse_markdown(file: &str) -> String {
-    let mut options = Options::empty();
-    options.insert(Options::ENABLE_YAML_STYLE_METADATA_BLOCKS);
-    
-    let parser = pulldown_cmark::Parser::new_ext(file, options);
+fn parse_markdown(content: &str) -> String {
+    let parser = pulldown_cmark::Parser::new(content);
 
     let mut html_output = String::new();
     pulldown_cmark::html::push_html(&mut html_output, parser);
-    
+
     html_output.replace(".md", ".html")
 }
