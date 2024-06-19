097f130401f88acbae7fa15061b3a4cb45488872
legoraft <legoraft@legoraft.com>
2024-06-19 22:32:01 +0200

    chore: removed implementation comment

--- src/frontmatter/parser.rs
+++ src/frontmatter/parser.rs
@@ -15,27 +15,6 @@ pub fn check_value(index: &str, yaml: &Yaml) -> String {
     value.to_string()
 }
 
-/*
-Design:
-
-Get frontmatter from gray_matter with parse.matter()
-
-Parse frontmatter from yaml. Check if value exists, if exists add value in struct. Otherwise add {tag} to struct.
-
-iterate over all possible values with for loop. Have variable; let values: ["title", "date", "description", "image", "tags"];
-
-Usage:
-let title = check_value("title"); (Would result in either String or {title} tag)
-
-Implementation:
-fn check_value(value: &str) -> String {
-    if value.isbad {
-        return format("{value}");
-    }
-    return yaml[value].tostring;
-}
-*/
-
 #[cfg(test)]
 mod tests {
     use super::*;
