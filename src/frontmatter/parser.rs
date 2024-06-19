4999205eed0556e403797e8a4f5786aa71c3dd0b
legoraft <legoraft@legoraft.com>
2024-06-19 22:25:50 +0200

    feat: added design and check_value

--- src/frontmatter/parser.rs
+++ src/frontmatter/parser.rs
@@ -2,16 +2,43 @@ use std::io::Error;
 
 use yaml_rust2::{Yaml, YamlLoader};
 
+use super::*;
+
 fn parse(frontmatter: String) -> Vec<Yaml> {
     let yaml = YamlLoader::load_from_str(&frontmatter).unwrap();
     
     yaml
 }
 
-fn check_value(index: &str, yaml: Vec<Yaml>) -> Result<String, Error> {
-    let yaml = yaml[0];
+fn check_value(index: &str, yaml: Yaml) -> String {
+    if yaml[index].is_badvalue() {
+        return format!("{:?}", index);
+    }
     
+    let value: &str = yaml[index].as_str().unwrap();
+    value.to_string()
 }
+
+/*
+Design:
+
+Get frontmatter from gray_matter with parse.matter()
+
+Parse frontmatter from yaml. Check if value exists, if exists add value in struct. Otherwise add {tag} to struct.
+
+iterate over all possible values with for loop. Have variable; let values: ["title", "date", "description", "image", "tags"];
+
+Usage:
+let title = check_value("title"); (Would result in either String or {title} tag)
+
+Implementation:
+fn check_value(value: &str) -> String {
+    if value.isbad {
+        return format("{value}");
+    }
+    return yaml[value].tostring;
+}
+*/
 
 #[cfg(test)]
 mod tests {
