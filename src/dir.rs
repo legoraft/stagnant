a979b5476222f37151fe30e6e25d1c81c9d33be6
legoraft <legoraft@legoraft.com>
2024-06-21 16:53:22 +0200

    fix: made frontmatter parsing not interfere with content

--- src/dir.rs
+++ src/dir.rs
@@ -13,17 +13,21 @@ fn duplicate_template() {
     let site_path = Path::new("./site");
     
     template::copy_directory(template_path, site_path).expect("Couldn't copy template!");
+    
+    let posts_template = fs::read_to_string("./site/posts/[id].html").expect("Post template doesn't exist.");
+    
+    fs::remove_file("./site/posts/[id].html").expect("Couldn't delete post template!");
 }
 
 fn parse_frontmatter(template: String) {
     let (frontmatter, content) = split_markdown(file);
     
-    let html = template.replace("{content}", &parse_markdown(&content))
-        .replace("{title}", &frontmatter.title)
+    let html = template.replace("{title}", &frontmatter.title)
         .replace("{date}", &frontmatter.date)
         .replace("{description}", &frontmatter.description)
         .replace("{image}", &frontmatter.image)
-        .replace("{tags}", &frontmatter.tags);
+        .replace("{tags}", &frontmatter.tags)
+        .replace("{content}", &parse_markdown(&content));
 }
 
 fn write_html() {
