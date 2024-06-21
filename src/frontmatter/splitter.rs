6841ec247adc3cdeeae6e46507aad06dff617a35
legoraft <legoraft@legoraft.com>
2024-06-21 17:19:56 +0200

    feat: added custom frontmatter splitter

--- src/frontmatter/splitter.rs
+++ src/frontmatter/splitter.rs
@@ -1,9 +1,46 @@
-
-
 pub fn split_post(file: String) -> (String, String) {
-    let yaml = get_yaml(frontmatter_result.matter);
-    let frontmatter = parse(yaml);
-    let content = frontmatter_result.content;
+    let mut matter: String = String::new();
+    let mut content: String = String::new();
     
-    (frontmatter, content)
+    let lines: Vec<&str> = file.lines().collect();
+    let mut in_matter = false;
+    
+    if &lines[0] == &"---" {
+        in_matter = true;
+    }
+    
+    for line in &lines[1..] {
+        if in_matter && line == &"---" {
+            in_matter = false;
+            continue;
+        } else if in_matter {
+            matter.push_str(line);
+        } else {
+            content.push_str(line);
+        }
+    }
+    
+    (matter, content)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_split() {
+        let file = "\
+---
+title: \"Hello, world!\"
+date: \"2024-06-21\"
+---
+
+This is some content to test if we also get this.
+
+I also want an extra line to check if it works completely.".to_string();
+        
+        let (matter, content) = ("title: \"Hello, world!\"\ndate: \"2024-06-21\"".to_string(), "\n\nThis is some content to test if we also get this.\n\nI also want an extra line to check if it works completely.".to_string());
+        
+        assert_eq!((matter, content), split_post(file));
+    }
 }
